0e195846775b7c024d7a28c007d8033d6c99f801
Marcin Lewandowski <marcin@radiokit.org>
2017-02-06 13:45:00 +0100

    Added mix compiler, added detection of current platform if no --platform is set

--- lib/bundlex/platform.ex
+++ lib/bundlex/platform.ex
@@ -25,26 +25,56 @@ defmodule Bundlex.Platform do
   def get_platform_from_opts!(opts) do
     cond do
       platform = opts[:platform] ->
-        {String.to_atom(platform), get_module!(platform)}
+        platform_name = String.to_atom(platform)
+        {platform_name, get_module!(platform_name)}
 
       true ->
-        Mix.raise "Cannot create bundle for unspecified platform. Please pass platform platform as --platform option."
+        get_current_platform!()
+    end
+  end
+
+
+  @doc """
+  Detects current platform.
+
+  In case of success returns tuple `{platform, platform_module}`.
+
+  Otherwise raises Mix error.
+  """
+  @spec get_current_platform! :: {platform_name_t, module}
+  def get_current_platform! do
+    case :os.type do
+      {:win32, _} ->
+        {:ok, reg} = :win32reg.open([:read])
+        :ok = :win32reg.change_key(reg, '\\hklm\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion')
+        {:ok, build} = :win32reg.value(reg, 'BuildLabEx')
+
+        platform_name = if build |> to_string |> String.contains?("amd64") do
+          :windows64
+        else
+          :windows32
+        end
+        :ok = :win32reg.close(reg)
+
+        {platform_name, get_module!(platform_name)}
+
+      # TODO add detection for more platforms
     end
   end
 
 
   defp get_module!(platform_name) do
     case platform_name do
-      "windows32" ->
+      :windows32 ->
         Bundlex.Platform.Windows32
 
-      "windows64" ->
+      :windows64 ->
         Bundlex.Platform.Windows64
 
-      "android_armv7" ->
+      :android_armv7 ->
         Bundlex.Platform.AndroidARMv7
 
-      "unix64" ->
+      :unix64 ->
         Bundlex.Platform.Unix64
 
       _ ->
--- lib/tasks/compile.bundlex.build.lib.ex
+++ lib/tasks/compile.bundlex.build.lib.ex
@@ -1,4 +1,4 @@
-defmodule Mix.Tasks.Bundlex.Build.Lib do
+defmodule Mix.Tasks.Compile.Bundlex.Build.Lib do
   use Mix.Task
   alias Bundlex.Makefile
 
@@ -8,11 +8,18 @@ defmodule Mix.Tasks.Bundlex.Build.Lib do
   """
 
   @shortdoc "Builds a library for the given platform"
-  @switches [platform: :string]
+  @switches [
+    platform: :string,
+    "no-deps": :string,
+    "no-archives-check": :string,
+    "no-elixir-version-check": :string,
+    "no-warnings-as-errors": :string,
+  ]
+
   @spec run(OptionParser.argv) :: :ok
   def run(args) do
     # Parse options
-    {opts, _} = OptionParser.parse!(args, aliases: [t: :platform], strict: @switches)
+    {opts, _} = OptionParser.parse!(args, aliases: [t: :platform], switches: @switches)
 
     {platform, platform_module} = Bundlex.Platform.get_platform_from_opts!(opts)
     Mix.shell.info "Building for platform #{platform}"
