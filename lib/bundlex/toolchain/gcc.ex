5b7bcea58c7f09c4c2d94e0b9fa0a434eddaf74d
Marcin Lewandowski <marcin@radiokit.org>
2017-04-19 16:57:30 +0200

    gcc: Build each source file separately, link separately

--- lib/bundlex/toolchain/gcc.ex
+++ lib/bundlex/toolchain/gcc.ex
@@ -10,13 +10,36 @@ defmodule Bundlex.Toolchain.GCC do
     # FIXME escape quotes properly
 
     includes_part = includes |> Enum.map(fn(include) -> "-I\"#{include}\"" end) |> Enum.join(" ")
-    sources_part = sources |> Enum.map(fn(source) -> "\"c_src/#{source}\"" end) |> Enum.join(" ")
     libs_part = libs |> Enum.map(fn(lib) -> "-l#{lib}" end) |> Enum.join(" ")
-    pkg_configs_part = pkg_configs |> Enum.map(fn(pkg_config) ->
-      %Porcelain.Result{status: 0, out: out} = Porcelain.exec("pkg-config", ["--cflags", "--libs", pkg_config])
+    
+    pkg_config_libs_part = pkg_configs |> Enum.map(fn(pkg_config) ->
+      %Porcelain.Result{status: 0, out: out} = Porcelain.exec("pkg-config", ["--libs", pkg_config])
       out |> String.trim
     end) |> Enum.join(" ")
 
-    ["gcc -fPIC -W -rdynamic -undefined -shared -o #{output}.so #{includes_part} #{libs_part} #{pkg_configs_part} #{sources_part}"]
+    pkg_config_cflags_part = pkg_configs |> Enum.map(fn(pkg_config) ->
+      %Porcelain.Result{status: 0, out: out} = Porcelain.exec("pkg-config", ["--cflags", pkg_config])
+      out |> String.trim
+    end) |> Enum.join(" ")
+
+    objects = sources |> Enum.map(fn(source) -> object_path(source) end) |> Enum.join(" ")
+
+ 
+    commands_sources =
+      sources
+      |> Enum.map(fn(source) ->
+        "gcc -fPIC -W -O2 -g #{includes_part} #{libs_part} #{pkg_config_cflags_part} \"#{source_path(source)}\" -c -o \"#{object_path(source)}\""
+      end)
+
+    commands_linker =
+      ["gcc -rdynamic -undefined -shared #{objects} #{libs_part} #{pkg_config_libs_part} -o #{output}.so"]
+
+    commands_sources ++ commands_linker
   end
+
+
+  defp source_path(source), do: "c_src/#{source}"
+
+
+  defp object_path(source), do: "c_src/#{source}.o" |> String.replace(~r(\.c\.o$), ".o")
 end
