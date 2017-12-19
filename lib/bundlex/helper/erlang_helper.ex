e2688d3fc03580a744d3c1b2046a9056c5aeb44f
Mateusz Front <mateusz.front@swmansion.com>
2017-12-19 18:59:49 +0100

    improved erlang path resolving

--- lib/bundlex/helper/erlang_helper.ex
+++ lib/bundlex/helper/erlang_helper.ex
@@ -11,28 +11,6 @@ defmodule Bundlex.Helper.ErlangHelper do
   Tries to determine path to includes directory of locally installed Erlang.
   """
   @spec get_includes!(atom) :: String.t
-  def get_includes!(:windows32), do: get_includes_windows!()
-  def get_includes!(:windows64), do: get_includes_windows!()
+  def get_includes!(_platform), do: Path.join [:code.root_dir, "usr", "include"]
 
-  def get_includes!(:macosx) do
-    # Assumes that user has erlang installed via brew
-    Bundlex.Helper.DirectoryHelper.wildcard("/usr/local/Cellar/erlang/*/lib/erlang/usr/include/")
-  end
-
-  def get_includes!(:linux) do
-    # FIXME
-    Bundlex.Helper.DirectoryHelper.wildcard("/usr/local/erlang/usr/include/")
-  end
-
-
-  defp get_includes_windows! do
-    # FIXME should be a wildcard
-    case DirectoryHelper.wildcard("c:\\Program Files\\erl8.3\\erts-8.3\\include") do
-      nil ->
-        Mix.raise "Unable to determine location of Erlang include dir (do you have Erlang 19.0 installed?)"
-
-      directory ->
-        directory
-    end
-  end
 end
