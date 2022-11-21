7c587fec58bb1e1982b4fed37705859b5e69fadd
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-11-21 15:35:25 +0100

    Example documented code

--- test_projects/example/c_src/example/bar_nif.c
+++ test_projects/example/c_src/example/bar_nif.c
@@ -1,18 +1,25 @@
+/** @file */
+
 #include <erl_nif.h>
 #include <example_lib/example_lib_nif.h>
 
-static ERL_NIF_TERM export_bar(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-    (void)argc;
-    int a, b;
-    enif_get_int(env, argv[0], &a);
-    enif_get_int(env, argv[0], &b);
-    return enif_make_int(env, sub(a, b));
+/**
+ * @brief NIF function to call the C function from the example_lib library.
+ * Subtracts first element from itself.
+ *
+ * @param argc Unused.
+ * @param argv Only the first element is used.
+ * @return ERL_NIF_TERM
+ */
+static ERL_NIF_TERM export_bar(ErlNifEnv* env, int argc,
+                               const ERL_NIF_TERM argv[]) {
+  (void)argc;
+  int a, b;
+  enif_get_int(env, argv[0], &a);
+  enif_get_int(env, argv[0], &b);
+  return enif_make_int(env, sub(a, b));
 }
 
-static ErlNifFunc nif_funcs[] =
-{
-    {"bar", 2, export_bar, 0}
-};
+static ErlNifFunc nif_funcs[] = {{"bar", 2, export_bar, 0}};
 
 ERL_NIF_INIT(Elixir.ExampleTest.Bar.Nif, nif_funcs, NULL, NULL, NULL, NULL)
--- test_projects/example/c_src/example/foo_nif.c
+++ test_projects/example/c_src/example/foo_nif.c
@@ -1,18 +1,25 @@
+/** @file */
+
 #include <erl_nif.h>
 #include <example_lib/example_lib_nif.h>
 
-static ERL_NIF_TERM export_foo(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-    (void)argc;
-    int a, b;
-    enif_get_int(env, argv[0], &a);
-    enif_get_int(env, argv[0], &b);
-    return enif_make_int(env, add(a, b));
+/**
+ * @brief NIF function to call the C function from the example_lib library. Adds
+ * first element to itself.
+ *
+ * @param argc Unused.
+ * @param argv Only the first element is used.
+ * @return ERL_NIF_TERM
+ */
+static ERL_NIF_TERM export_foo(ErlNifEnv* env, int argc,
+                               const ERL_NIF_TERM argv[]) {
+  (void)argc;
+  int a, b;
+  enif_get_int(env, argv[0], &a);
+  enif_get_int(env, argv[0], &b);
+  return enif_make_int(env, add(a, b));
 }
 
-static ErlNifFunc nif_funcs[] =
-{
-    {"foo", 2, export_foo, 0}
-};
+static ErlNifFunc nif_funcs[] = {{"foo", 2, export_foo, 0}};
 
 ERL_NIF_INIT(Elixir.ExampleTest.Foo.Nif, nif_funcs, NULL, NULL, NULL, NULL)
