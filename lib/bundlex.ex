723110cb25b09a9d0739f3b860dc355057655af1
varsill <lukasz.kita0@gmail.com>
2023-09-04 16:19:40 +0200

    Add get_target()

--- lib/bundlex.ex
+++ lib/bundlex.ex
@@ -8,6 +8,27 @@ defmodule Bundlex do
 
   @type platform_t :: :linux | :macosx | :windows32 | :windows64
 
+  @typedoc """
+  A tuple of three elements describing the platform.
+
+  It consists of:
+  * architecture - e.g. `x86_64` or `arm64`
+  * vendor - e.g. `pc`
+  * operating system - e.g. `linux` or `freebsd`
+  """
+  @type target_triplet ::
+          {architecture :: String.t(), vendor :: String.t(), operating_system :: String.t()}
+  @doc """
+  A function returning a target triplet for the environment on which it is run.
+  """
+  @spec get_target() :: target_triplet()
+  def get_target() do
+    [architecture, vendor, os | _rest] =
+      :erlang.system_info(:system_architecture) |> List.to_string() |> String.split("-")
+
+    {architecture, vendor, os}
+  end
+
   @doc """
   Returns current platform name.
   """
