31dabd3a64c43f600d320e62244282a8881da82e
Mateusz Front <mateusz.front@swmansion.com>
2023-10-30 20:14:19 +0100

    refactor os_deps API

--- lib/bundlex/toolchain/common/unix/os_deps.ex
+++ lib/bundlex/toolchain/common/unix/os_deps.ex
@@ -8,11 +8,22 @@ defmodule Bundlex.Toolchain.Common.Unix.OSDeps do
   def resolve_os_deps(native) do
     {cflags_list, libs_list} =
       native.os_deps
-      |> Enum.map(fn {provider_or_providers, lib_name_or_names} ->
-        providers = Bunch.listify(provider_or_providers)
-        lib_names = Bunch.listify(lib_name_or_names)
+      |> Enum.map(fn
+        {name, providers} when is_atom(name) and name != :pkg_config ->
+          resolve_os_dep(name, native.app, Bunch.listify(providers), [])
 
-        resolve_single_os_dep(providers, lib_names, native.app)
+        {providers, lib_names} ->
+          name = lib_names |> Bunch.listify() |> Enum.join("-") |> String.to_atom()
+
+          providers =
+            providers
+            |> Bunch.listify()
+            |> Enum.map(fn
+              {:precompiled, url} -> {:precompiled, url, lib_names}
+              :pkg_config -> {:pkg_config, lib_names}
+            end)
+
+          resolve_os_dep(name, native.app, providers, [])
       end)
       |> Enum.unzip()
 
@@ -26,144 +37,167 @@ defmodule Bundlex.Toolchain.Common.Unix.OSDeps do
     }
   end
 
-  defp resolve_single_os_dep([], lib_names, _app) do
-    raise "Couldn't load OS dependencies for libraries: #{inspect(lib_names)}."
+  defp resolve_os_dep(name, app, [], errors) do
+    Output.raise("""
+    Couldn't load OS dependency #{inspect(name)} of package #{app}. \
+    Make sure to follow installation instructions that may be available in the readme of #{app}.
+
+    Tried the following providers:
+
+    #{errors |> Enum.reverse() |> Enum.join("\n")}
+    """)
   end
 
-  defp resolve_single_os_dep(providers, lib_names, app) do
-    [first_provider | rest_of_providers] = providers
+  defp resolve_os_dep(name, app, [provider | providers], errors) do
+    case resolve_os_dep_provider(name, provider) do
+      {:ok, cflags, libs} ->
+        {cflags, libs}
 
-    case first_provider do
-      nil ->
-        resolve_single_os_dep(rest_of_providers, lib_names, app)
+      {:error, reason} ->
+        resolve_os_dep(name, app, providers, [
+          "Provider `#{inspect(provider)}` #{reason}" | errors
+        ])
+    end
+  end
 
-      :pkg_config ->
-        try do
-          {get_flags_for_pkg_config(lib_names, :cflags, app),
-           get_flags_for_pkg_config(lib_names, :libs, app)}
-        rescue
-          e ->
-            IO.warn(
-              "Couldn't load #{inspect(lib_names)} libraries with pkg-config due to: #{inspect(e)}."
-            )
+  defp resolve_os_dep_provider(name, :pkg_config) do
+    resolve_os_dep_provider(name, {:pkg_config, "#{name}"})
+  end
 
-            resolve_single_os_dep(rest_of_providers, lib_names, app)
-        end
+  defp resolve_os_dep_provider(_name, {:pkg_config, pkg_configs}) do
+    pkg_configs = Bunch.listify(pkg_configs)
 
-      {:precompiled, precompiled_dependency_url} ->
-        case maybe_download_precompiled_package(precompiled_dependency_url) do
-          :unavailable ->
-            resolve_single_os_dep(rest_of_providers, lib_names, app)
+    with {:ok, cflags} <- get_flags_from_pkg_config(pkg_configs, :cflags),
+         {:ok, libs} <- get_flags_from_pkg_config(pkg_configs, :libs) do
+      {:ok, cflags, libs}
+    end
+  end
 
-          package_path ->
-            {get_flags_for_precompiled({{:precompiled, package_path}, lib_names}, :cflags),
-             get_flags_for_precompiled({{:precompiled, package_path}, lib_names}, :libs)}
-        end
+  defp resolve_os_dep_provider(name, {:precompiled, url}) do
+    resolve_os_dep_provider(name, {:precompiled, url, "#{name}"})
+  end
+
+  defp resolve_os_dep_provider(name, {:precompiled, url, lib_names}) do
+    lib_names = Bunch.listify(lib_names)
+
+    with {:ok, package_path} <-
+           maybe_download_precompiled_package(name, url) do
+      {:ok, get_flags_for_precompiled(package_path, lib_names, :cflags),
+       get_flags_for_precompiled(package_path, lib_names, :libs)}
     end
   end
 
   defp get_precompiled_path(), do: "#{Mix.Project.build_path()}/bundlex_precompiled/"
 
-  defp get_flags_for_precompiled(
-         {{:precompiled, precompiled_dependency_path}, lib_names},
-         flags_type
-       ) do
-    case flags_type do
-      :libs ->
-        full_packages_library_path = Path.absname("#{precompiled_dependency_path}/lib")
+  defp get_flags_for_precompiled(precompiled_dependency_path, lib_names, :libs) do
+    full_packages_library_path = Path.absname("#{precompiled_dependency_path}/lib")
 
-        [
-          "-L#{full_packages_library_path}",
-          "-Wl,-rpath,#{full_packages_library_path}"
-        ] ++
-          Enum.map(lib_names, &"-l#{remove_lib_prefix(&1)}")
-
-      :cflags ->
-        full_include_path = Path.absname("#{precompiled_dependency_path}/include")
-        ["-I#{full_include_path}"]
-    end
+    [
+      "-L#{full_packages_library_path}",
+      "-Wl,-rpath,#{full_packages_library_path}"
+    ] ++
+      Enum.map(lib_names, &"-l#{remove_lib_prefix(&1)}")
   end
 
-  defp get_flags_for_pkg_config(lib_names, flags_type, app) do
-    flags_type = "--#{flags_type}"
-    System.put_env("PATH", System.get_env("PATH", "") <> ":/usr/local/bin:/opt/homebrew/bin")
+  defp get_flags_for_precompiled(precompiled_dependency_path, _lib_names, :cflags) do
+    full_include_path = Path.absname("#{precompiled_dependency_path}/include")
+    ["-I#{full_include_path}"]
+  end
 
-    case System.cmd("which", ["pkg-config"]) do
-      {_path, 0} ->
-        :ok
+  defp get_flags_from_pkg_config(pkg_configs, flags_type) do
+    try do
+      flags_type = "--#{flags_type}"
+      System.put_env("PATH", System.get_env("PATH", "") <> ":/usr/local/bin:/opt/homebrew/bin")
 
-      {_path, _error} ->
-        Output.raise("""
-        pkg-config not found. Bundlex needs pkg-config to find packages in system.
-        On Mac OS, you can install pkg-config via Homebrew by typing `brew install pkg-config`.
-        """)
-    end
+      case System.cmd("which", ["pkg-config"]) do
+        {_path, 0} ->
+          :ok
 
-    Enum.map(lib_names, fn lib_name ->
-      case System.cmd("pkg-config", [flags_type, lib_name], stderr_to_stdout: true) do
-        {output, 0} ->
-          String.trim_trailing(output)
-
-        {output, error} ->
-          Output.raise("""
-          Couldn't find system library #{lib_name} with pkg-config. Check whether it's installed.
-          Installation instructions may be available in the readme of package #{app}.
-          Output from pkg-config:
-          Error: #{error}
-          #{output}
-          """)
+        {_path, _error} ->
+          raise BundlexError, """
+          pkg-config not found. Bundlex needs pkg-config to find packages in system.
+          On Mac OS, you can install pkg-config via Homebrew by typing `brew install pkg-config`.
+          """
       end
-    end)
+
+      pkg_configs
+      |> Enum.map(fn pkg_config ->
+        case System.cmd("pkg-config", [flags_type, pkg_config], stderr_to_stdout: true) do
+          {output, 0} ->
+            String.trim_trailing(output)
+
+          {output, error} ->
+            raise BundlexError, """
+            pkg-config error:
+            Code: #{error}
+            #{output}
+            """
+        end
+      end)
+      |> then(&{:ok, &1})
+    rescue
+      e ->
+        {:error,
+         """
+         couldn't load #{inspect(pkg_configs)} libraries with pkg-config due to:
+         #{format_exception(e)}
+         """}
+    end
   end
 
   defp remove_lib_prefix("lib" <> libname), do: libname
   defp remove_lib_prefix(libname), do: libname
 
-  defp maybe_download_precompiled_package(precompiled_dependency_url) do
-    get_precompiled_path() |> File.mkdir_p!()
-    package_path = get_package_path(precompiled_dependency_url)
+  defp maybe_download_precompiled_package(name, url) do
+    precompiled_path = get_precompiled_path()
+    File.mkdir_p!(precompiled_path)
+    package_path = "#{precompiled_path}#{Zarex.sanitize(url)}"
 
-    cond do
-      package_path == :unavailable ->
-        :unavailable
+    if File.exists?(package_path) do
+      {:ok, package_path}
+    else
+      File.mkdir!(package_path)
 
-      File.exists?(package_path) ->
-        package_path
+      try do
+        temporary_destination = "#{get_precompiled_path()}/temporary"
+        download(url, temporary_destination)
 
-      true ->
-        File.mkdir!(package_path)
+        {_output, 0} =
+          System.shell("tar -xf #{temporary_destination} -C #{package_path} --strip-components 1")
 
-        try do
-          temporary_destination = "#{get_precompiled_path()}/temporary"
-          download(precompiled_dependency_url, temporary_destination)
+        File.rm!(temporary_destination)
+        {:ok, package_path}
+      rescue
+        e ->
+          File.rm_rf!(package_path)
 
-          {_output, 0} =
-            System.shell(
-              "tar -xf #{temporary_destination} -C #{package_path} --strip-components 1"
-            )
-
-          File.rm!(temporary_destination)
-          package_path
-        rescue
-          e ->
-            IO.warn("Couldn't download and extract the dependency due to: #{inspect(e)}.")
-            :unavailable
-        end
+          {:error,
+           """
+           couldn't download and extract the precompiled dependency #{inspect(name)} due to:
+           #{format_exception(e)}
+           """}
+      end
     end
-  end
-
-  defp get_package_path(:unavailable), do: :unavailable
-
-  defp get_package_path(url) do
-    "#{get_precompiled_path()}#{Zarex.sanitize(url)}"
   end
 
   defp download(url, dest) do
     response = Req.get!(url)
 
     case response.status do
-      200 -> File.write!(dest, response.body)
-      _other -> raise "Cannot download file from #{url}. Repsonse status: #{response.status}"
+      200 ->
+        File.write!(dest, response.body)
+
+      _other ->
+        raise BundlexError, """
+        Cannot download file from #{url}
+        Response status: #{response.status}
+        """
     end
+  end
+
+  defp format_exception(exception) do
+    Exception.format(:error, exception)
+    |> String.trim()
+    |> String.replace(~r/^/m, "\t")
   end
 end
