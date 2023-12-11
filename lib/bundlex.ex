239eaab74c67cffdc927791f40ebd093672fc2c1
Noarkhh <jakub.pryc@gmail.com>
2023-12-11 15:24:47 +0100

    Extract abi from :erlang.system_info and add it to target spec

--- lib/bundlex.ex
+++ lib/bundlex.ex
@@ -9,27 +9,29 @@ defmodule Bundlex do
   @type platform_t :: :linux | :macosx | :windows32 | :windows64 | :nerves
 
   @typedoc """
-  A map containing three fields that describe the platform.
+  A map containing four fields that describe the platform.
 
   It consists of:
   * architecture - e.g. `x86_64` or `arm64`
   * vendor - e.g. `pc`
   * os - operating system, e.g. `linux` or `darwin20.6.0`
+  * abi (optional) - application binary interface, e.g. `musl` or `gnu`
   """
   @type target ::
-          %{architecture: String.t(), vendor: String.t(), os: String.t()}
+          %{architecture: String.t(), vendor: String.t(), os: String.t(), abi: String.t() | nil}
   @doc """
   A function returning a target triplet for the environment on which it is run.
   """
   @spec get_target() :: target()
   def get_target() do
-    [architecture, vendor, os | _rest] =
+    [architecture, vendor, os | maybe_abi] =
       :erlang.system_info(:system_architecture) |> List.to_string() |> String.split("-")
 
     %{
       architecture: architecture,
       vendor: vendor,
-      os: os
+      os: os,
+      abi: List.first(maybe_abi)
     }
   end
 
@@ -55,5 +57,35 @@ defmodule Bundlex do
   @spec build_path(application :: atom, native :: atom, native_interface :: atom) :: String.t()
   def build_path(application \\ MixHelper.get_app!(), native, native_interface) do
     Bundlex.Toolchain.output_path(application, native, native_interface)
+  end
+
+  @spec default_os_dep_config(os_dep_name :: atom()) :: Bundlex.Project.os_dep()
+  def default_os_dep_config(os_dep_name) do
+    precompiled_package_url_prefix =
+      "https://github.com/membraneframework-precompiled/precompiled_#{os_dep_name}/releases/latest/download/#{os_dep_name}"
+
+    precompiled_package_url =
+      case Bundlex.get_target() do
+        %{os: "linux", vendor: "alpine"} ->
+          nil
+
+        %{os: "linux"} ->
+          "#{precompiled_package_url_prefix}_linux.tar.gz"
+
+        %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
+          "#{precompiled_package_url_prefix}_macos_intel.tar.gz"
+
+        %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
+          "#{precompiled_package_url_prefix}_macos_arm.tar.gz"
+
+        _other ->
+          nil
+      end
+
+    if precompiled_package_url != nil do
+      {os_dep_name, [{:precompiled, precompiled_package_url}, :pkg_config]}
+    else
+      {os_dep_name, [:pkg_config]}
+    end
   end
 end
