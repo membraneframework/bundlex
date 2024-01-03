0f71809a5ffca42562cf1a933a4e7a3a02fc7697
Mateusz Front <mateusz.front@swmansion.com>
2024-01-03 14:56:05 +0100

    update test dep urls

--- test_projects/example/bundlex.exs
+++ test_projects/example/bundlex.exs
@@ -10,15 +10,25 @@ defmodule Example.BundlexProject do
   defp get_ffmpeg() do
     url =
       case Bundlex.get_target() do
+        %{abi: "musl"} ->
+          nil
+
+        %{architecture: "aarch64", os: "linux"} ->
+          "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2023-11-30-12-55/ffmpeg-n6.0.1-linuxarm64-gpl-shared-6.0.tar.xz"
+
         %{os: "linux"} ->
-          "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n4.4-latest-linux64-gpl-shared-4.4.tar.xz/"
+          "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2023-11-30-12-55/ffmpeg-n6.0.1-linux64-gpl-shared-6.0.tar.xz"
 
         %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
           "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/latest/download/ffmpeg_macos_intel.tar.gz"
 
+        %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
+          "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/latest/download/ffmpeg_macos_arm.tar.gz"
+
         _other ->
           nil
       end
+
 
     [{:precompiled, url, ["libswscale", "libavcodec"]}]
   end
