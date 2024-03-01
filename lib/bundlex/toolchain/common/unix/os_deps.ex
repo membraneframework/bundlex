25fce44d75092bd2dfc3daf319775e1d4d15d3ba
Michał Śledź <michalsledz34@gmail.com>
2024-03-01 17:45:42 +0100

    Replace realpath with Path.expand

--- lib/bundlex/toolchain/common/unix/os_deps.ex
+++ lib/bundlex/toolchain/common/unix/os_deps.ex
@@ -140,8 +140,7 @@ defmodule Bundlex.Toolchain.Common.Unix.OSDeps do
     # are stored because we need to assure that the relative link in the symlink's target
     # will work properly also when the symlink is put in the location, for which the
     # logical path differs from the physical path
-    {physical_output_path, 0} = System.shell("realpath #{logical_output_path}")
-    physical_output_path = String.trim_trailing(physical_output_path)
+    physical_output_path = logical_output_path |> Path.expand() |> String.trim_trailing()
     physical_dep_dir_name = logical_dep_dir_name <> "_physical"
     create_relative_symlink(lib_path, physical_output_path, physical_dep_dir_name)
 
